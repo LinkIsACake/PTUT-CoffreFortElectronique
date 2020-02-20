09c8996bf10b489541c6ba72a33d54b4a05e6e8d
randomniichan <leokolo@hotmail.fr>
2020-02-20 17:29:51 +0100

    ajout des DAOs

---
+++ DAOs/DAO.py
@@ -0,0 +1,10 @@
+from src.DbHelper import DbHelper
+
+class DAO:
+    database = None
+
+    def __init__(self):
+        self.database = DbHelper()
+
+    def close(self):
+        self.database.close()

517e7ac27da04254ffa8550c24c00afd63b54e46
lsudreau <leo.sudreau@gmail.com>
2020-03-01 22:38:19 +0100

    fix create file if not exist

--- src/Dao/UserDAO.py
+++ src/Dao/UserDAO.py
@@ -1,16 +1,17 @@
 from .DAO import DAO
 import sqlite3
 
+
 class UserDAO(DAO):
     database = None
-    
-    def __init__(self, path:str="../databases/users.sqlite"):
-        DAO.__init__(self,path)
+
+    def __init__(self, path: str = "../ressource/users.sqlite"):
+        DAO.__init__(self, path)
 
     def close(self):
         DAO.close(self)
 
-    def fetchUser(self, username:str):
+    def fetchUser(self, username: str):
         self.database.query("SELECT * FROM Users WHERE username = ?", [username])
         result = self.database.getFirstResult()
         if result != None:
@@ -18,16 +19,16 @@ class UserDAO(DAO):
         else:
             return False
 
-    def createUser(self, username:str, password:str):
+    def createUser(self, username: str, password: str):
         try:
-            self.database.query("INSERT INTO Users VALUES (?,?)", [username,password])
+            self.database.query("INSERT INTO Users VALUES (?,?)", [username, password])
             self.database.connection.commit()
             return True
         except sqlite3.Error as e:
             print("An error has occured:", e.args[0])
             return False
 
-    def checkCredentials(self, username:str, password:str):
+    def checkCredentials(self, username: str, password: str):
         self.database.query("SELECT * FROM Users WHERE username = ? AND password = ?", [username, password])
         result = self.database.getFirstResult()
         return (result != None)

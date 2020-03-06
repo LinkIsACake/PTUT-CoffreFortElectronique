44b9da6b7b221d066e68c9437f713e6255e883a5
lsudreau <leo.sudreau@gmail.com>
2020-03-06 02:26:02 +0100

    Label begin

---
+++ src/assets/Label.py
@@ -0,0 +1,21 @@
+class Label:
+
+    # LOGIN VIEW
+
+    LOGIN_BUTTON = "Connexion"
+    REGISTRATION_BUTTON = "Inscription"
+
+    LOGIN_INPUT_USERNAME = "Nom d'utilisateur"
+    LOGIN_INPUT_PASSWORD = "Mot de passe"
+
+    EXISTING_USERS = "Utilisateurs existants"
+
+    REGISTER_DONE = "Inscription réussie"
+    WRONG_CREDENTIAL = "Mot-de-passe ou Nom d'utilisateur incorrect"
+    PATH_EXIST = "Un dossier correspond déjâ à ce nom utilisateur mais il n'est pas enregisté, veuillez supprimer le dossier"
+    WRONG_INPUT = "Mot-de-passe ou Nom d'utilisateur non valide"
+    ACCOUNT_EXISTING = "Compte déjâ existant"
+
+    INFO = "Info"
+    ERROR = "Erreur"
+    # HOME VIEW
--- src/views/Login.py
+++ src/views/Login.py
@@ -2,6 +2,7 @@ from PyQt5.QtWidgets import QPushButton, QLineEdit, QMessageBox, QGridLayout, QL
 from PyQt5 import QtWidgets
 
 from src.controllers import MainController
+from src.assets.Label import Label
 
 
 class UserList(QListWidget):
@@ -13,13 +14,15 @@ class UserList(QListWidget):
         self.addItem(user)
 
 
+
+
 class Login(QtWidgets.QDialog):
     layout: QGridLayout
 
     controller: MainController
 
     login_button: QPushButton
-    register_button : QPushButton
+    register_button: QPushButton
 
     register: QPushButton
     password_input: QLineEdit
@@ -37,8 +40,9 @@ class Login(QtWidgets.QDialog):
         self.setGeometry(300, 300, 250, 150)
         self.layout = QGridLayout()
         self.layout.setColumnStretch(10, 10)
-        self.login_button = QPushButton("Connexion")
-        self.register_button = QPushButton("Inscription")
+        self.login_button = QPushButton(Label.LOGIN_BUTTON)
+
+        self.register_button = QPushButton(Label.REGISTRATION_BUTTON)
 
         self.login_button.pressed.connect(self.login)
         self.register_button.pressed.connect(self.register)
@@ -48,9 +52,9 @@ class Login(QtWidgets.QDialog):
         self.password_input = QLineEdit()
         self.password_input.setEchoMode(QLineEdit.Password)
 
-        self.layout.addWidget(QLabel("Nom d'utilisateur"), 0, 0)
+        self.layout.addWidget(QLabel(Label.LOGIN_INPUT_USERNAME), 0, 0)
 
-        self.layout.addWidget(QLabel("Mot-de-passe"), 1, 0)
+        self.layout.addWidget(QLabel(Label.REGISTRATION_BUTTON), 1, 0)
 
         self.layout.addWidget(self.username_input, 0, 1)
         self.layout.addWidget(self.password_input, 1, 1)
@@ -58,7 +62,7 @@ class Login(QtWidgets.QDialog):
         self.layout.addWidget(self.login_button, 2, 0)
         self.layout.addWidget(self.register_button, 2, 1)
 
-        self.layout.addWidget(QLabel("Utilisateurs existants"), 0, 3)
+        self.layout.addWidget(QLabel(Label.EXISTING_USERS), 0, 3)
 
         self.user_list = UserList()
         self.user_list.clicked.connect(self.select_user)
@@ -72,6 +76,8 @@ class Login(QtWidgets.QDialog):
         user_selected = self.user_list.selectedItems()[0]
 
         self.username_input.setText(user_selected.text())
+
+
 
     def update_list_user(self):
         self.users = self.controller.get_users()
@@ -89,32 +95,24 @@ class Login(QtWidgets.QDialog):
     def connected(self):
         self.close()
 
-    def register_ok(self):
-        QMessageBox.about(self, "Inscription", "Inscription réussie")
+    def notif_about_info(self, message: str):
+        QMessageBox.about(self, Label.INFO, message)
 
-    def wrong_credential(self):
-        QMessageBox.about(self, "Erreur", "Mot-de-passe ou Nom d'utilisateur incorrect")
-
-    def path_exist(self):
-        QMessageBox.about(self, "Erreur",
-                          "Un dossier correspond déjâ à ce nom utilisateur mais il n'est pas enregisté, veuillez supprimer le dossier")
-
-    def wrong_input(self):
-        QMessageBox.about(self, "Erreur", "Mot-de-passe ou Nom d'utilisateur incorrect")
+    def notif_about_error(self, message):
+        QMessageBox.about(self, Label.ERROR, message)
 
     def notify(self, **kwargs):
         if kwargs.get("connected", False):
-            self.register_ok()
             self.close()
         else:
             if kwargs.get("register", False):
-                self.register_ok()
+                self.notif_about_info(Label.REGISTER_DONE)
             else:
-                QMessageBox.about(self, "Compte déjâ existant")
+                self.notif_about_error(Label.ACCOUNT_EXISTING)
             if kwargs.get("path_exist", False):
-                self.path_exist()
+                self.notif_about_error(Label.PATH_EXIST)
             if kwargs.get("wrong_credential", False):
-                self.wrong_credential()
+                self.notif_about_error(Label.WRONG_CREDENTIAL)
             if kwargs.get("wrong_input", False):
-                self.wrong_input()
+                self.notif_about_error(Label.WRONG_INPUT)
         self.update_list_user()
